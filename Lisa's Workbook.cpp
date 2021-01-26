d9180354cb84ddc3e0aea6341baa76d3d3399010
prakash <64192704+mewebbie@users.noreply.github.com>
2021-01-26 13:52:42 +0530

    Add files via upload

---
+++ Lisa's Workbook.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+int main()
+{
+    int n,k;
+    cin >> n >> k;
+    int j,spl=0,i,page=1,arr[n+1];
+
+    for(i=1;i<=n;i++) cin >> arr[i];
+    for(i=1;i<=n;i++){
+        for(j=1;j<=arr[i];j++){
+            if(j==page) spl+=1; 
+            if(j%k==0 && j/k>0) {
+                if(j!=arr[i]) page+=1;
+            }
+        }
+        page+=1;
+    }
+    cout << spl;
+    
+    return 0;
+}
